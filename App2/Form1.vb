a52907943a8c7050467fa779e3eab8fb9685bb0e
MrCoult <80452894+MrCoult@users.noreply.github.com>
2021-03-11 13:10:21 +0200

    Add files via upload

---
+++ App2/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim number As Integer
+
+        Randomize()
+        ''Acest program genereaza un numar aleatoriu intre 0 si 50
+        number = Int(Rnd() * 50) + 1
+
+        TextBox1.Text = number
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+End Class
